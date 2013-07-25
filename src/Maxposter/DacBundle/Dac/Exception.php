bdd95f7438b857589f54a6b68d0a4efc4815be00
avorobiev <avorobiev@maxposter.ru>
2013-07-25 10:08:05 +0400

    Рефакторинг slqFilter

--- src/Maxposter/DacBundle/Dac/Exception.php
+++ src/Maxposter/DacBundle/Dac/Exception.php
@@ -4,5 +4,7 @@ namespace Maxposter\DacBundle\Dac;
 
 class Exception extends \Exception
 {
-
+    const
+        ERR_SQL_FILTER = 10
+    ;
 }
--- src/Maxposter/DacBundle/Dac/SqlFilter.php
+++ src/Maxposter/DacBundle/Dac/SqlFilter.php
@@ -4,7 +4,6 @@ namespace Maxposter\DacBundle\Dac;
 use \Doctrine\ORM\Mapping\ClassMetadata;
 use \Doctrine\DBAL\Connection;
 use Maxposter\DacBundle\Annotations\Mapping\Service\Annotations;
-use Maxposter\DacBundle\Exceptions\DacMappingException;
 
 /**
  * @package Maxposter\DacBundle\SqlFilter
@@ -16,24 +15,64 @@ class SqlFilter extends \Doctrine\ORM\Query\Filter\SQLFilter
         /** @var Settings */
         $dacSettings,
 
-        /** @var \Maxposter\DacBundle\Annotations\Mapping\Service\Annotations */
-        $annotations,
+        /** @var Annotations */
+        $annotations
+    ;
 
-        /** @var \Doctrine\ORM\Mapping\ClassMetadata */
-        $targetEntity;
-
+    /**
+     * Установка объекта со значениями DAC-аннотаций
+     *
+     * @param Annotations $annotations
+     */
     public function setAnnotations(Annotations $annotations)
     {
         $this->annotations = $annotations;
     }
 
+    /**
+     * Получение объекта со значениями DAC-аннотаций
+     *
+     * @return Annotations
+     * @throws Exception
+     * fixme: Нужен тест
+     */
+    private function getAnnotation()
+    {
+        if (is_null($this->annotations)) {
+            throw new Exception('Ошибка в инициализации SQL-фильтра: не задан объект с аннотациями.', Exception::ERR_SQL_FILTER);
+        }
+
+        return $this->annotations;
+    }
+
+    /**
+     * Установка контейнера с параметрами фильтрации данных
+     *
+     * @param Settings $dacSettings
+     */
     public function setDacSettings(Settings $dacSettings)
     {
         $this->dacSettings = $dacSettings;
     }
 
     /**
-     * Gets the SQL query part to add to a query.
+     * Получение контейнера с параметрами фильтрации данных
+     *
+     * @return Settings
+     * @throws Exception
+     * fixme: Нужен тест
+     */
+    private function getDacSettings()
+    {
+        if (is_null($this->dacSettings)) {
+            throw new Exception('Ошибка в инициализации SQL-фильтра: не заданы параметры фильтрации.', Exception::ERR_SQL_FILTER);
+        }
+
+        return $this->dacSettings;
+    }
+
+    /**
+     * Модификация SQL-запроса
      *
      * @param  \Doctrine\ORM\Mapping\ClassMetadata $targetEntity
      * @param  string $targetTableAlias
@@ -42,16 +81,16 @@ class SqlFilter extends \Doctrine\ORM\Query\Filter\SQLFilter
     public function addFilterConstraint(ClassMetadata $targetEntity, $targetTableAlias)
     {
         $entityName = $targetEntity->getReflectionClass()->getName();
-        $this->targetEntity = $targetEntity;
 
-        if (!$this->annotations->hasDacFields($entityName)) {
+        $annotation = $this->getAnnotation();
+        if (!$annotation->hasDacFields($entityName)) {
             return '';
         }
 
-        $dacFields = $this->annotations->getDacFields($entityName);
+        $dacFields = $annotation->getDacFields($entityName);
         $conditions = array();
         foreach ($dacFields as $filteredFieldName => $dacSettingsName) {
-            $filteredColumnName = $this->getColumnNameByField($filteredFieldName);
+            $filteredColumnName = $this->getColumnName($filteredFieldName, $targetEntity);
             $dacSettingsValue= $this->getDacSettings()->get($dacSettingsName);
             if ($dacSettingsValue)
             {
@@ -73,29 +112,28 @@ class SqlFilter extends \Doctrine\ORM\Query\Filter\SQLFilter
         return $result;
     }
 
-    private function getDacSettings()
+    /**
+     * Получение названия поля в таблице в БД по названию параметра в сущности
+     *
+     * @param string $fieldName     Название параметра в сущности
+     * @param ClassMetadata $targetEntity
+     * @return string Искомое       Название поля в БД
+     * @throws Exceptions
+     * fixme: Нужны тесты на разное поведение метода
+     */
+    private function getColumnName($fieldName, ClassMetadata $targetEntity)
     {
-        if (is_null($this->dacSettings)) {
-            throw new Exception('Ошибка в инициализации SQL-фильтра: не заданы параметры фильтрации');
-        }
-
-        return $this->dacSettings;
-    }
-
-    private function getColumnNameByField($fieldName)
-    {
-        if (in_array($fieldName, $this->targetEntity->getFieldNames())) {
-            $columnNames = $this->targetEntity->columnNames;
-            $columnName = $columnNames[$fieldName];
-        } elseif (in_array($fieldName, $this->targetEntity->getAssociationNames())) {
-            $associationMappings = $this->targetEntity->associationMappings;
+        if (in_array($fieldName, $targetEntity->getFieldNames())) {
+            $columnName =  $targetEntity->getColumnName($fieldName);
+        } elseif (in_array($fieldName, $targetEntity->getAssociationNames())) {
+            $associationMappings = $targetEntity->associationMappings;
 
             if (count($associationMappings[$fieldName]['joinColumnFieldNames']) !== 1) {
-                throw new DacMappingException();
+                throw new Exception(sprintf('Для свойства %s связь реализована по составному ключу. Такое поведение не поддерживается DAC.', $fieldName), Exception::ERR_SQL_FILTER);
             }
             $columnName = array_pop($associationMappings[$fieldName]['joinColumnFieldNames']);
         } else {
-            throw new DacMappingException("ClassMetadata не содержит информацию о названии столбца в таблице для свойства " . $fieldName);
+            throw new Exception(sprintf('ClassMetadata не содержит информацию о названии столбца в таблице для свойства %s.', $fieldName), Exception::ERR_SQL_FILTER);
         }
 
         return $columnName;
